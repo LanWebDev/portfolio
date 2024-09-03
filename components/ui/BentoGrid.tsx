15286f150191931594fc39c7a46397730251a4a8
LanWebDev <149800661+LanWebDev@users.noreply.github.com>
2024-09-03 16:18:28 +0200

    fix

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -76,7 +76,7 @@ export const BentoGridItem = ({
     requestIdleCallback(() => {
       const timer = setTimeout(() => {
         setGlobe(<GridGlobe />);
-      }, 1000); // Delay by 3 seconds
+      }, 1500); // Delay by 3 seconds
       return () => clearTimeout(timer);
     });
   }, []);
